eb382ebfdb866a00661a1d968fd3bd4991440967
Jamie Francese <jamienf@gmail.com>
2015-01-03 12:51:02 -0500

    Manufacturer code cleanup and better navigation

--- spec/features/cars/user_creates_a_new_car_spec.rb
+++ spec/features/cars/user_creates_a_new_car_spec.rb
@@ -1,19 +1,22 @@
 require "rails_helper"
 
 feature "User adds a new car listing", %q(
-    As a car salesperson
-    I want to record a newly acquired car
-    So that I can list it in my lot
-    
-    Acceptance Criteria:
+  As a car salesperson
+  I want to record a newly acquired car
+  So that I can list it in my lot
 
-    [ ] I must specify the manufacturer, color, year, and mileage of the car (an association between the car and an existing manufacturer should be created).
-    [ ] Only years from 1920 and above can be specified.
-    [ ] I can optionally specify a description of the car.
-    [ ] If I enter all of the required information in the required formats, the car is recorded and I am presented with a notification of success.
-    [ ] If I do not specify all of the required information in the required formats, the car is not recorded and I am presented with errors.
-    [ ] Upon successfully creating a car, I am redirected back to the index of cars.
+  Acceptance Criteria:
+
+  [ ] I must specify the manufacturer, color, year, and mileage of the car (an association between the car and an existing manufacturer should be created).
+  [ ] Only years from 1920 and above can be specified.
+  [ ] I can optionally specify a description of the car.
+  [ ] If I enter all of the required information in the required formats, the car is recorded and I am presented with a notification of success.
+  [ ] If I do not specify all of the required information in the required formats, the car is not recorded and I am presented with errors.
+  [ ] Upon successfully creating a car, I am redirected back to the index of cars.
 
   ) do
+    context "User creates new car listing"
+      scenario ""
+
 
   end
--- spec/features/manufacturers/user_creates_a_manufacturer_spec.rb
+++ spec/features/manufacturers/user_creates_a_manufacturer_spec.rb
@@ -1,15 +1,15 @@
 require "rails_helper"
 
 feature "A user saves a manufacturers information", %q(
-As a car salesperson
-I want to record a car manufacturer
-So that I can keep track of the types of cars found in the lot
+  As a car salesperson
+  I want to record a car manufacturer
+  So that I can keep track of the types of cars found in the lot
 
-Acceptance Criteria:
+  Acceptance Criteria:
 
-[x] I must specify a manufacturer name and country.
-[x] If I do not specify the required information, I am presented with errors.
-[x] If I specify the required information, the manufacturer is recorded and I am redirected to the index of manufacturers
+  [x] I must specify a manufacturer name and country.
+  [x] If I do not specify the required information, I am presented with errors.
+  [x] If I specify the required information, the manufacturer is recorded and I am redirected to the index of manufacturers
 ) do
 
   scenario "user successfully adds manufacturer information" do
